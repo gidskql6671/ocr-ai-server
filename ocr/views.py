9ca160af77ca25356e32ddf91eea5d4cb2b747c8
gidskql6671 <gidskql6671@gmail.com>
2023-04-13 14:36:45 +0900

    띄어쓰기 추가

--- ocr/views.py
+++ ocr/views.py
@@ -5,6 +5,6 @@ from . import ocr_module, spell_checker
 def ocr_image(request):
     ocr_result = ocr_module.ocr(request.GET['path'])
 
-    spell_check_result = spell_checker.check(''.join(ocr_result))
+    spell_check_result = spell_checker.check(' '.join(ocr_result))
 
     return JsonResponse(spell_check_result)
