f77133e4e0c7228813e4db76c737c789a9e0a6f5
gidskql6671 <gidskql6671@gmail.com>
2023-05-23 02:06:54 +0900

    ocr with correct text 양식 수정

--- ocr/urls.py
+++ ocr/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.ocr_image, name="ocr"),
-    path("<str:correct_text>", views.ocr_image_with_correct, name="ocr-with-correct"),
 ]
--- ocr/views.py
+++ ocr/views.py
@@ -1,19 +1,17 @@
 from django.http import JsonResponse
 from . import ocr_module, spell_checker
+from urllib import parse
 
 
 def ocr_image(request):
     ocr_result = ocr_module.ocr(request.GET['path'])
 
-    spell_check_result = spell_checker.check(' '.join(ocr_result))
+    correct_text = request.GET['correctText']
+
+    if correct_text is None:
+        spell_check_result = spell_checker.check(' '.join(ocr_result))
+    else:
+        correct_text = parse.unquote(correct_text)
+        spell_check_result = spell_checker.check_with_correct(' '.join(ocr_result), correct_text)
 
     return JsonResponse(spell_check_result)
-
-
-def ocr_image_with_correct(request, correct_text):
-    ocr_result = ocr_module.ocr(request.GET['path'])
-
-    spell_check_result = spell_checker.check_with_correct(' '.join(ocr_result), correct_text)
-
-    return JsonResponse(spell_check_result)
-
