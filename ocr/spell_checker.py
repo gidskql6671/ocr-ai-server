ecff582a788ed9cf6bc93698871da8ad9fa38d54
gidskql6671 <gidskql6671@gmail.com>
2023-05-30 22:50:58 +0900

    정답률 알고리즘 개선

--- ocr/spell_checker.py
+++ ocr/spell_checker.py
@@ -19,45 +19,40 @@ def check(input_str):
 
 
 def check_with_correct(input_str, correct_str):
-    i = 0
-    j = 0
+    input_str_len = len(input_str)
+    correct_str_len = len(correct_str)
 
-    checked_count = 0
-    error_count = 0
-    while i < len(input_str) and j < len(correct_str):
-        if input_str[i] == ' ':
-            i += 1
-            continue
-        if correct_str[j] == ' ':
-            j += 1
-            continue
+    if input_str == correct_str:
+        answer_percent = 1.
+    else:
+        lcs_len = get_lcs_len(input_str, correct_str, input_str_len, correct_str_len)
 
-        checked_count += 1
-        if input_str[i] != correct_str[j]:
-            error_count += 1
-        i += 1
-        j += 1
-
-    while i < len(input_str):
-        if input_str[i] == ' ':
-            i += 1
-            continue
-        error_count += 1
-        checked_count += 1
-        i += 1
-    while j < len(correct_str):
-        if correct_str[j] == ' ':
-            j += 1
-            continue
-        error_count += 1
-        checked_count += 1
-        j += 1
-
-    print(checked_count)
-    print(error_count)
+        if lcs_len < correct_str_len:
+            answer_percent = lcs_len / correct_str_len
+        else:
+            answer_percent = correct_str_len / input_str_len
 
     return {
-        "answer_percent": (checked_count - error_count) / checked_count,
+        "answer_percent": answer_percent,
         "correct_string": correct_str,
         "origin_string": input_str
     }
+
+
+def get_lcs_len(s1, s2, m, n):
+    dp = [[0 for _ in range(n + 1)] for _ in range(2)]
+
+    i = 0
+    prev_i = 1
+    for s1_index in range(m + 1):
+        for j in range(n + 1):
+            if s1_index == 0 or j == 0:
+                dp[i][j] = 0
+            elif s1[s1_index - 1] == s2[j - 1]:
+                dp[i][j] = dp[prev_i][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[prev_i][j], dp[i][j - 1])
+        prev_i = i
+        i = (i + 1) % 2
+
+    return dp[prev_i][n]
